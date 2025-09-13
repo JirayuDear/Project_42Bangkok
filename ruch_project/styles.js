783416a340a467d1b78532a386a3d5b1f7b7b404
Jirapol-pongthong <jirapol.phongtong@gmail.com>
2025-09-13 11:55:09 +0700

    commit

--- ruch_project/styles.js
+++ ruch_project/styles.js
@@ -1,13 +1,9 @@
-document.getElementById("menu-toggle")?.addEventListener("click", () => {
-  document.getElementById("main-nav").classList.toggle("open");
-});
 
-// ปุ่มสุ่มสี
 document.getElementById("random-color-btn")?.addEventListener("click", () => {
   const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
   document.documentElement.style.setProperty("--accent", randomColor);
 
-  // เพิ่ม alpha สำหรับสีเข้ม
+
   let darkColor = randomColor + "cc";
   document.documentElement.style.setProperty("--accent-dark", darkColor);
 });
